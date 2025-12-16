326978e1905c0d7e32c3f32b819e0dc0faffeaff
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-12-16 16:27:28 +0400

    feat: fetch products from database

--- script/index.js
+++ script/index.js
@@ -1,6 +1,17 @@
 function main() {
   const demo = document.getElementById("demo");
   demo.textContent = "we are starting";
+  fetchProducts();
 }
 
 main();
+
+async function fetchProducts() {
+  try {
+    const response = await fetch("https://dummyjson.com/carts/12");
+    const data = await response.json();
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+  }
+}
