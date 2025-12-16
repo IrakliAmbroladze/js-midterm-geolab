b4602ab5871c6c3327523ee5a2cc61d42be6d3d1
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-12-16 19:13:20 +0400

    feat: calculate totalProducts, totalQuantity, totalPrice

--- script/index.js
+++ script/index.js
@@ -1,15 +1,14 @@
 async function main() {
-  const demo = document.getElementById("demo");
-  demo.textContent = "we are starting";
   const products = await fetchProducts();
   createProductList(products);
+  createCartSummary(products);
 }
 
 main();
 
 async function fetchProducts() {
   try {
-    const response = await fetch("https://dummyjson.com/carts/15");
+    const response = await fetch("https://dummyjson.com/carts/12");
     const data = await response.json();
     return data.products;
   } catch (err) {
@@ -22,16 +21,26 @@ function createProductList(products) {
   products.forEach((product) => {
     const { title, price, quantity, total, thumbnail } = product;
     const li = document.createElement("li");
+    li.classList.add("product-container");
     li.innerHTML = `
-		<img src="${thumbnail}" alt="${title}" width="100px" height="100px"/>
+		<img src="${thumbnail}" alt="${title}" />
 		<h3>${title}</h3>
 		<div>${price}</div>
-		<button>-</button>
+		<button style="height: 20px">-</button>
 		${quantity}
-		<button>+</button>
+		<button style="height: 20px">+</button>
 		${total}
-		<button>x</button>
+		<button  style="height: 20px">x</button>
 		`;
     productList.appendChild(li);
   });
+}
+
+function createCartSummary(products) {
+  const totalProducts = products.length;
+  const totalQuantity = products.reduce(
+    (acc, { quantity }) => acc + quantity,
+    0,
+  );
+  const totalPrice = products.reduce((acc, { total }) => acc + total, 0);
 }
