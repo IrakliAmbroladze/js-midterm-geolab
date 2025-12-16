c304335b975b9df4ef1935d0ea77f32dc320f86a
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-12-16 18:19:11 +0400

    feat: render product-list

--- script/index.js
+++ script/index.js
@@ -1,17 +1,37 @@
-function main() {
+async function main() {
   const demo = document.getElementById("demo");
   demo.textContent = "we are starting";
-  fetchProducts();
+  const products = await fetchProducts();
+  createProductList(products);
 }
 
 main();
 
 async function fetchProducts() {
   try {
-    const response = await fetch("https://dummyjson.com/carts/12");
+    const response = await fetch("https://dummyjson.com/carts/15");
     const data = await response.json();
-    console.log(data);
+    return data.products;
   } catch (err) {
     console.log(err);
   }
+}
+
+function createProductList(products) {
+  const productList = document.getElementById("product-list");
+  products.forEach((product) => {
+    const { title, price, quantity, total, thumbnail } = product;
+    const li = document.createElement("li");
+    li.innerHTML = `
+		<img src="${thumbnail}" alt="${title}" width="100px" height="100px"/>
+		<h3>${title}</h3>
+		<div>${price}</div>
+		<button>-</button>
+		${quantity}
+		<button>+</button>
+		${total}
+		<button>x</button>
+		`;
+    productList.appendChild(li);
+  });
 }
